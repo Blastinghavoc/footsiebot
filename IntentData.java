f5c4bbec357c547c75b661ae3636b96c158857f5
Nicola <N.Branchini@warwick.ac.uk>
2018-02-07 13:29:09 +0000

    Add files via upload

---
+++ IntentData.java
@@ -0,0 +1,27 @@
+package footsiebot.intelligencecore;
+
+public class IntentData {
+
+  private Intent intent;
+  private double[] values;
+  private float priority;
+  private float getIrrelevantSuggestionWeight;
+
+
+  public Intent getIntent() {
+
+  }
+
+  public float getPriority() {
+
+  }
+
+  public void incrementPriority(float p) {
+
+  }
+
+  public void decrementPriority(float p) {
+
+  }
+
+}
