5420f0c32c91f5aebf9e400d6c0638f1fd9e2fa3
Nicola <N.Branchini@warwick.ac.uk>
2018-02-07 14:07:16 +0000

    Complete IIintelligenceUnit

--- intelligencecore/IIntelligenceUnit.java
+++ intelligencecore/IIntelligenceUnit.java
@@ -2,7 +2,22 @@ package footsiebot.intelligencecore;
 
 
 public interface IIntelligenceUnit {
+     public String getSuggestion(ParseResult pr);
 
+
+
+   public String onUpdatedDatabase();
+
+   public void onShutdown();
+   /**
+    * User has reported that a suggestion has not been relevant
+    * ajust weights accordingly
+    * @param  String companyOrGroup
+    * @return
+    */
+   public String onSuggestionIrrelevant(String companyOrGroup);
+
+   public String onNewsTime();
 
 
 
