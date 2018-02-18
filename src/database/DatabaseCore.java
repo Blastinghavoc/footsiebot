7014ebfd39a35b72e234d19b88de86bf4e089c69
Blastinghavoc <J.Taylor.24@warwick.ac.uk>
2018-02-18 13:23:54 +0000

    Fixed conflicts

--- src/database/DatabaseCore.java
+++ src/database/DatabaseCore.java
@@ -329,11 +329,7 @@ public class DatabaseCore implements IDatabaseManager {
       query+= "NATURAL JOIN CompanyAbsoluteChangeCount ";
       query+= "NATURAL JOIN CompanyClosingPriceCount ";
       query+= "NATURAL JOIN PercentageChangeCount ";
-<<<<<<< HEAD
-      //System.out.println(query);
-=======
-      //System.out.println(query);//DEBUG
->>>>>>> origin
+
       Statement stmt = null;
       ResultSet rs = null;
 
