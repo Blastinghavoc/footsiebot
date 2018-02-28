ad7f3a89f7eb84c7ed776bfa802743e22f9b9263
Blastinghavoc <J.Taylor.24@warwick.ac.uk>
2018-02-28 09:24:17 +0000

    Added voice input

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
-javac -encoding UTF8 -cp "./src/datagathering/jsoup.jar"  -d ./classes ./src/*.java ./src/nlp/*.java ./src/database/*.java ./src/datagathering/*.java ./src/gui/*.java ./src/ai/*.java
-java -cp "./classes";"./src/database/sqlite-jdbc-3.21.0.jar";"./src/datagathering/jsoup.jar" footsiebot.Core
+javac -encoding UTF8 -cp "./src/datagathering/jsoup.jar";"./src/voce-0.9.1/lib/voce.jar" -d ./classes ./src/*.java ./src/nlp/*.java ./src/database/*.java ./src/datagathering/*.java ./src/gui/*.java ./src/ai/*.java
+
+java -cp "./classes";"./src/database/sqlite-jdbc-3.21.0.jar";"./src/datagathering/jsoup.jar";"./src/voce-0.9.1/lib/voce.jar" -mx256m footsiebot.Core
