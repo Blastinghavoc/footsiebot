8bdd6aca77f126b476ad5be7f129067a61736bb7
Blastinghavoc <J.Taylor.24@warwick.ac.uk>
2018-03-07 14:21:38 +0000

    Removed nlp test

--- tests/runNLPtest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-javac -cp "./src/datagathering/jsoup.jar":"./src/voce-0.9.1/lib/voce.jar"  -d ./classes ./src/*.java ./src/nlp/*.java ./src/database/*.java ./src/datagathering/*.java ./src/gui/*.java ./src/ai/*.java
-java -cp "./classes":"./src/database/sqlite-jdbc-3.21.0.jar":"./src/datagathering/jsoup.jar":"./src/voce-0.9.1/lib/voce.jar" footsiebot.Core nlp
